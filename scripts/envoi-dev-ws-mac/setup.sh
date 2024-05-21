03b8d47387d9ac862550804033161864f21f8c7c
John Whitson <john.whitson@gmail.com>
2024-05-20 23:49:37 -0600

    Added console output to the setup script

--- scripts/envoi-dev-ws-mac/setup.sh
+++ scripts/envoi-dev-ws-mac/setup.sh
@@ -1,121 +1,119 @@
 #!/bin/bash
+
+console() {
+  echo -e "\033[0;21m[ENVOI SETUP] $1\033[0m"
+}
+
+clone_repo() {
+  repo_url=$1
+  # App name is the basename without the ext of the URL
+  app_name=${2:-$(basename "$repo_url" .git)}
+  console "Cloning $app_name..."
+  git clone $repo_url
+  console "Finished cloning $app_name."
+}
+
+install_app() {
+  command=$1
+  app_name=${2:-$1}
+  console "Installing $app_name..."
+  eval $command
+  console "Finished installing $app_name."
+}
+
+asdf_install_plugin() {
+  plugin=$1
+  version=$2
+  description=${3:-$1}
+  source=${4}
+
+  console "Installing $description"
+  asdf plugin add $plugin $source
+  asdf install $plugin $version
+  asdf global $plugin $version
+  console "Finished installing $description"
+}
+
 if command -v brew &> /dev/null
 then
+  console "Updating Homebrew"
   brew update
+  console "Finished updating Homebrew"
 else
-  # Not Found - Install
+  verbose "Homebrew is not installed"
+  console "Installing Homebrew"
   NONINTERACTIVE=1 /bin/bash -c \
      "$(curl -fsSL \
         https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
   (echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> /Users/ec2-user/.zprofile
   eval "$(/opt/homebrew/bin/brew shellenv)"
+  console "Finished installing Homebrew"
 fi
 
-brew install wget
-brew install mas
-brew install jq
-brew install asdf
-asdf_exec_path=$(brew --prefix asdf)/libexec/asdf.sh
+install_app "brew install wget" "wget"
+install_app "brew install mas" "mas"
+install_app "brew install jq" "jq"
+install_app "brew install asdf" "asdf"
+
+# Configure adsf to run on shell startup
+asdf_exec_path="$(brew --prefix asdf)/libexec/asdf.sh"
 echo -e "\n. ${asdf_exec_path}" >> ${ZDOTDIR:-~}/.zshrc
 . "${asdf_exec_path}"
 
-asdf plugin add awscli
-asdf install awscli latest:2
-asdf global awscli latest:2
+asdf_install_plugin  awscli latest:2 "AWS CLI"
+asdf_install_plugin  aws-sam-cli latest:1 "AWS SAM CLI"
+asdf_install_plugin carthage latest:0 "Carthage"
+asdf_install_plugin gcloud latest "Google Cloud SDK" "https://github.com/jthegedus/asdf-gcloud"
+asdf_install_plugin nodejs latest:20 "Node.js"
+asdf_install_plugin ruby latest:3 "Ruby" "https://github.com/asdf-vm/asdf-ruby.git"
 
-asdf plugin add aws-sam-cli
-asdf install aws-sam-cli latest:1
-asdf global aws-sam-cli latest:1
+install_app "brew install azure-cli" "Azure CLI"
 
-brew install azure-cli
-#asdf plugin-add azure-cli https://github.com/itspngu/asdf-azure-cli
-#asdf install azure-cli latest
-#asdf global azure-cli latest
+install_app "brew install --cask android-studio" "Android Studio"
+install_app "brew install --cask betterdisplay" "BetterDisplay"
+install_app "brew install --cask docker" "Docker"
+install_app "brew install --cask git-credential-manager" "Git Credential Manager"
+install_app "brew install --cask mongodb-compass" "MongoDB Compass"
+install_app "brew install --cask nosql-workbench" "NoSQL Workbench"
+install_app "brew install --cask visual-studio-code" "Visual Studio Code"
 
-asdf plugin add carthage
-asdf install carthage latest:0
-asdf global carthage latest:0
+install_app "brew install --cask dcv-viewer" "DCV Viewer"
+install_app "brew install --cask github" "GitHub"
+install_app "brew install --cask mongodb-compass" "MongoDB Compass"
+install_app "brew install --cask mysqlworkbench" "MySQL Workbench"
+install_app "brew install --cask obs" "OBS"
+install_app "brew install --cask pgadmin4" "pgAdmin"
+install_app "brew install --cask royal-tsx" "Royal TSX"
+install_app "brew install --cask streamlabs" "Streamlabs"
 
-asdf plugin add gcloud https://github.com/jthegedus/asdf-gcloud
-asdf install gcloud latest
-asdf global gcloud latest
-
-asdf plugin add nodejs
-asdf install nodejs latest:20
-asdf global nodejs latest:20
-
-asdf plugin add ruby https://github.com/asdf-vm/asdf-ruby.git
-asdf install ruby latest:3
-asdf global ruby latest:3
-
-brew install --cask android-studio
-brew install --cask betterdisplay
-brew install --cask docker
-brew install --cask git-credential-manager
-brew install --cask mongodb-compass
-brew install --cask nosql-workbench
-brew install --cask visual-studio-code
-
-brew install --cask dcv-viewer
-brew install --cask github
-brew install --cask mongodb-compass
-brew install --cask mysqlworkbench
-brew install --cask obs
-brew install --cask pgadmin4
-brew install --cask royal-tsx
-brew install --cask streamlabs
-
+# Download Tizen Studio - Web IDE. The certificate does not pass validation so we need to use --no-check-certificate
 wget --no-check-certificate \
-https://download.tizen.org/sdk/Installer/tizen-studio_5.6/web-ide_Tizen_Studio_5.6_macos-64.dmg \
-/home/ec2-user/Downloads/
+"https://download.tizen.org/sdk/Installer/tizen-studio_5.6/web-ide_Tizen_Studio_5.6_macos-64.dmg" \
+"/home/ec2-user/Downloads/"
 
-brew install ffmpeg
+install_app "brew install ffmpeg" "FFMPEG"
 
-gem install aspera-cli
+install_app "gem install aspera-cli" "Aspera CLI"
 
-sudo mkdir -p /opt/envoi
-sudo chown ec2-user:staff /opt/envoi
+install_app "sudo mkdir -p /opt/envoi"
+install_app "sudo chown ec2-user:staff /opt/envoi"
 
-cd "/opt/envoi"
+cd /opt/envoi
 
-# Envoi Transfer Service
-git clone https://github.com/envoi-io/envoi-transfer-service.git
+clone_repo "https://github.com/envoi-io/envoi-transfer-service.git" "Envoi Transfer Service"
+clone_repo "https://github.com/envoi-io/envoi-cloud-storage.git" "Envoi Cloud Storage CLI"
+clone_repo "https://github.com/envoi-io/envoi-cloud-desktops.git" "Envoi Cloud Desktops CLI"
+clone_repo "https://github.com/envoi-io/envoi-cloud-transcode.git" "Envoi Cloud Transcode CLI"
+clone_repo "https://github.com/envoi-io/envoi-mig.git" "Envoi MIG CLI"
+clone_repo "https://github.com/envoi-io/envoi-s3-client.git" "Envoi S3 Client CLI"
 
-# Envoi Cloud Storage CLI
-git clone https://github.com/envoi-io/envoi-cloud-storage.git
+install_app "brew install carthage" "Carthage"
+install_app "brew install gradle" "Gradle"
 
-# Cloud Desktops Project
-git clone https://github.com/envoi-io/envoi-cloud-desktops.git
+install_app "npm install -g cordova" "Cordova"
+install_app "npm install -g @ionic/cli" "Ionic"
 
-# Envoi Transcode
-git clone https://github.com/envoi-io/envoi-cloud-transcode.git
-
-# Mig
-git clone https://github.com/envoi-io/envoi-mig.git
-
-# Envoi S3 Client
-git clone https://github.com/envoi-io/envoi-s3-client.git
-
-brew install carthage
-brew install gradle
-
-npm install -g cordova
-npm install -g @ionic/cli
-
-# Enable Screen Sharing
 sudo launchctl enable system/com.apple.screensharing
 sudo launchctl load -w /System/Library/LaunchDaemons/com.apple.screensharing.plist
 
-# Modify macOS screen resolution on Mac instances
-#   https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/connect-to-mac-instance.html#mac-screen-resolution
-#   Install displayplacer - utility to change display settings
-brew tap jakehilborn/jakehilborn && brew install displayplacer
-
-#RES="2560x1600"
-#SCREEN_ID=$(displayplacer list | grep "Persistent screen id:" | awk '{print $4}')
-#displayplacer "id:${SCREEN_ID} res:${RES} scaling:off origin:(0,0) degree:0"
-
-# Configure for System Updates
-# https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/mac-instance-updates.html
-# sudo /usr/bin/dscl . -passwd /Users/ec2-user
-
+install_app "brew tap jakehilborn/jakehilborn && brew install displayplacer" "Displayplacer"
