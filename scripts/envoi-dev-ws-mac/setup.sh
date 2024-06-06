3437616f624e9a44ae9df947735f3488b9b1f551
John Whitson <john.whitson@gmail.com>
2024-06-06 13:10:00 -0600

    Updated setup script to run the install script

--- scripts/envoi-dev-ws-mac/setup.sh
+++ scripts/envoi-dev-ws-mac/setup.sh
@@ -23,7 +23,6 @@ get_instance_metadata() {
   path_part=$1
   TOKEN=`curl -s -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600"` \
   && curl -s -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/$path_part
-
 }
 
 get_instance_id() {
